264b1e94ead5fbfd7626c9c5f33b0785e7acec39
hasinajy <jyhasina@gmail.com>
2025-03-23 00:01:54 +0300

    chore: Remove javadoc from ticket expense service

---
+++ src/main/java/site/easy/to/build/crm/service/expense/TicketExpenseService.java
@@ -0,0 +1,21 @@
+package site.easy.to.build.crm.service.expense;
+
+import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+import site.easy.to.build.crm.entity.TicketExpense;
+import site.easy.to.build.crm.repository.expense.TicketExpenseRepository;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class TicketExpenseService {
+    private final TicketExpenseRepository ticketExpenseRepository;
+
+    public List<TicketExpense> getAllTicketExpenses() {
+        return ticketExpenseRepository.findAll();
+    }
+
+    public List<TicketExpense> getAllTicketExpensesByCustomerId(Integer customerId) {
+        return ticketExpenseRepository.findAllTicketExpensesByCustomerId(customerId);
+    }
+}
