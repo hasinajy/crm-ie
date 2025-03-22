43bc467755c354593e9d1afc5bd7b2fc1680a475
hasinajy <jyhasina@gmail.com>
2025-03-22 15:37:44 +0300

    refactor: Rename CustomerBudgetDtoRepository to CustomerBudgetRepository

--- src/main/java/site/easy/to/build/crm/service/budget/CustomerBudgetService.java
+++ src/main/java/site/easy/to/build/crm/service/budget/CustomerBudgetService.java
@@ -3,19 +3,19 @@ package site.easy.to.build.crm.service.budget;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 import site.easy.to.build.crm.dto.CustomerBudgetDto;
-import site.easy.to.build.crm.repository.CustomerBudgetDtoRepository;
+import site.easy.to.build.crm.repository.CustomerBudgetRepository;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CustomerBudgetService {
-    private final CustomerBudgetDtoRepository budgetDtoRepository;
+    private final CustomerBudgetRepository customerBudgetRepository;
 
     public List<CustomerBudgetDto> getAllBudgetDtos() {
-        return budgetDtoRepository.findAllBudgetDtos();
+        return customerBudgetRepository.findAllBudgetDtos();
     }
 
     public List<CustomerBudgetDto> getBudgetsByCustomerId(Long customerId) {
-        return budgetDtoRepository.findBudgetDtosByCustomerId(customerId);
+        return customerBudgetRepository.findBudgetDtosByCustomerId(customerId);
     }
 }
