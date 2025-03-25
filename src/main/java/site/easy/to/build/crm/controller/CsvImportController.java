b94ba1616ba93aec20f45228be999b0571f6ddc4
hasinajy <jyhasina@gmail.com>
2025-03-25 11:45:56 +0300

    feat: Implement data format check on customer CSV

--- src/main/java/site/easy/to/build/crm/controller/CsvImportController.java
+++ src/main/java/site/easy/to/build/crm/controller/CsvImportController.java
@@ -1,11 +1,23 @@
 package site.easy.to.build.crm.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import site.easy.to.build.crm.exception.InvalidCsvFormatException;
+import site.easy.to.build.crm.service.csv.CustomerCsvImportService;
 
 @Controller
 public class CsvImportController {
@@ -21,12 +33,44 @@ public class CsvImportController {
             @RequestParam("expensesCsvFile") MultipartFile expenseCsv,
             RedirectAttributes redirectAttributes) {
 
+        boolean hasError = false;
+        List<InvalidCsvFormatException> exceptions = new ArrayList<>();
+        String pageRedirect = "redirect:/csv-import";
+
         if (customerCsv.isEmpty() || expenseCsv.isEmpty()) {
+            hasError = true;
             redirectAttributes.addFlashAttribute("error",
                     "One or more files are missing. Please select all files to upload");
-            return "redirect:/csv-import";
+        } else {
+            try (
+                    Reader customerReader = new BufferedReader(new InputStreamReader(customerCsv.getInputStream()));
+                    Reader expenseReader = new BufferedReader(new InputStreamReader(expenseCsv.getInputStream()))) {
+
+                Iterable<CSVRecord> customerRecords = CSVFormat.DEFAULT.parse(customerReader);
+                Iterable<CSVRecord> expenseRecords = CSVFormat.DEFAULT.parse(expenseReader);
+
+                CustomerCsvImportService customerCsvImportService = new CustomerCsvImportService(
+                        customerCsv.getOriginalFilename(), customerRecords);
+                customerCsvImportService.processCustomerCsv();
+
+                if (customerCsvImportService.hasError()) {
+                    hasError = true;
+                    exceptions.addAll(customerCsvImportService.getExceptions());
+                }
+
+                // TODO: Process expense records
+            } catch (IOException e) {
+                hasError = true;
+            }
         }
 
-        return "redirect:/csv-import";
+        if (!hasError) {
+            redirectAttributes.addFlashAttribute("message", "CSV file uploaded and processed successfully!");
+        } else {
+            redirectAttributes.addFlashAttribute("invalidFormatErrors", exceptions);
+            redirectAttributes.addFlashAttribute("message", "Failed to process CSV files.");
+        }
+
+        return pageRedirect;
     }
 }
---
+++ src/main/java/site/easy/to/build/crm/service/csv/CustomerCsvImportService.java
@@ -0,0 +1,84 @@
+package site.easy.to.build.crm.service.csv;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.Data;
+import site.easy.to.build.crm.entity.Customer;
+import site.easy.to.build.crm.exception.InvalidCsvFormatException;
+
+@Service
+@Data
+public class CustomerCsvImportService {
+    private String filename;
+    private Iterable<CSVRecord> customerRecords;
+    private List<Customer> customers;
+    private List<InvalidCsvFormatException> exceptions;
+
+    /* ------------------------------ Constructors ------------------------------ */
+
+    @Autowired
+    public CustomerCsvImportService() {
+    }
+
+    public CustomerCsvImportService(String filename, Iterable<CSVRecord> customerRecords) {
+        this.setCustomerRecords(customerRecords);
+        this.setCustomers(new ArrayList<>());
+        this.setExceptions(new ArrayList<>());
+        this.setFilename(filename);
+    }
+
+    /* ----------------------------- Service methods ---------------------------- */
+
+    public void processCustomerCsv() {
+        int counter = 0;
+        for (CSVRecord customerRecord : this.getCustomerRecords()) {
+            if (counter > 0) {
+                Customer customer = parseCsvRecord(customerRecord, counter);
+                this.getCustomers().add(customer);
+            }
+            counter++;
+        }
+    }
+
+    public boolean hasError() {
+        return !this.getExceptions().isEmpty();
+    }
+
+    private Customer parseCsvRecord(CSVRecord customerRecord, int lineNumber) {
+        Customer customer = new Customer();
+
+        String email = customerRecord.get(0);
+        String name = customerRecord.get(1);
+
+        if (!isValidEmail(email)) {
+            this.getExceptions().add(
+                    new InvalidCsvFormatException(this.getFilename(), lineNumber, "Invalid email format provided"));
+        }
+
+        customer.setEmail(email);
+        customer.setName(name);
+
+        return customer;
+    }
+
+    /* -------------------------------- Utilities ------------------------------- */
+
+    private static boolean isValidEmail(String email) {
+        if (email == null || email.isEmpty()) {
+            return false;
+        }
+
+        String emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(email);
+
+        return matcher.matches();
+    }
+}
